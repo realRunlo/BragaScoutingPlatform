47b0231aafba02c3d0d426d0758f8d33f1b6ea8e
Gonçalo Braz Afonso <brazafonso2001@gmail.com>
2023-11-01 20:48:47 +0000

    fix - duplicate competitions/seasons in json

--- etl/scouting_data.py
+++ etl/scouting_data.py
@@ -128,7 +128,8 @@ def db_non_existent_players(players:list,db_handler:Db_handler):
 
 def extract_competitions_info(competitions:list):
     '''Extracts competitions metadata (id, seasons) from json config file'''
-    competitions_info = []
+    competitions_info = {}
+    competition_list = []
     area_competitions = []
     competitions_names = []
     last_area_code = None
@@ -157,7 +158,7 @@ def extract_competitions_info(competitions:list):
         if 'tm_code' in competition:
             c_i['custom_name'] = competition['tm_code']
         for season in competition['seasons']:
-            if 'wyId' in season:
+            if 'wyId' in season and season['wyId'] not in c_i['seasons']:
                 c_i['seasons'].append(season['wyId'])
             elif 'start' in season and 'end' in season:
                 start = season['start']
@@ -165,12 +166,15 @@ def extract_competitions_info(competitions:list):
                 seasons_list = get_seasons_competitions(c_i['wyId'])
                 seasonsId = [k for k,v in seasons_list.items() if v['startDate'][0:4] == start and v['endDate'][0:4] == end] #Neste momento so estamos a comparar os anos, se calhar deviamos ver isto melhor
                 for seasonId in seasonsId:
-                    c_i['seasons'].append(seasonId)
+                    if seasonId not in c_i['seasons']:
+                        c_i['seasons'].append(seasonId)
             else:
                 #Error
                 continue
-        competitions_info.append(c_i)
-    return competitions_info
+        competitions_info[c_i['wyId']]=c_i
+        if c_i['wyId'] not in competition_list:
+            competition_list.append(c_i['wyId'])
+    return [competitions_info[c] for c in competition_list]
 
 def prepare_areas_insert(areas):
     '''Inserts area into db'''
