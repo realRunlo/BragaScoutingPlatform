3c5dbd66dd12898c6cc78c4f5fd31b5104c18956
Tiago Silva <57015073+surumkata@users.noreply.github.com>
2023-11-04 17:15:44 +0000

    tm script fix

--- etl/tm_script.py
+++ etl/tm_script.py
@@ -8,12 +8,13 @@ from db import Db_handler
 import logging
 from utils import similarity
 from datetime import date
-
+import time
 
 def parse_arguments():
     '''Define and parse arguments using argparse'''
     parser = argparse.ArgumentParser(description='wyscout API request')
     parser.add_argument('--db_config','-dbc'            ,type=str, nargs=1,required=True                                , help='Db config json file path')
+    parser.add_argument('--complete_name','-cn'         ,action='store_true'                                            , help='Request complete name of player' )
     return parser.parse_args()
 
 today = date.today()
@@ -61,14 +62,32 @@ def like_multiple(where, likes):
 
 def extract_tm_id(url):
     # Use uma expressão regular para encontrar o ID no URL
-    match = re.search(r'/verein/(\d+)/saison_id/', url)
+    match = re.search(r'/verein/(\d+)/saison_id/(\d+)', url)
     
+    if match:
+        # Retorna o valor correspondente ao grupo 1 (o ID) da expressão regular
+        return int(match.group(1)),int(match.group(2))
+    else:
+        # Se não encontrar um ID, você pode retornar um valor padrão ou levantar uma exceção, conforme necessário
+        return None
+    
+def extract_p_tm_id(url):
+    match = re.search(r'/profil/spieler/(\d+)',url)
     if match:
         # Retorna o valor correspondente ao grupo 1 (o ID) da expressão regular
         return int(match.group(1))
     else:
         # Se não encontrar um ID, você pode retornar um valor padrão ou levantar uma exceção, conforme necessário
         return None
+
+def scrap_player_name(tm_id):
+    url = f"https://www.transfermarkt.pt/player/profil/spieler/{tm_id}"
+    pageTree = requests.get(url, headers=headers)
+    pageSoup = BeautifulSoup(pageTree.content, 'html.parser')
+
+    table = pageSoup.find("div", {"class" : "info-table info-table--right-space"})
+    complete_name = table.find("span",{"class" : "info-table__content info-table__content--bold"}).text if table else ""
+    return complete_name
 
 def scrap_official_name(tm_id):
     url = f"https://www.transfermarkt.com/club/datenfakten/verein/{tm_id}"
@@ -82,9 +101,9 @@ def scrap_official_name(tm_id):
     official_name = td.text
     return official_name
 
-def scrap_players(tm_id):
+def scrap_players(tm_id,season):
     players = {}
-    url = f"https://www.transfermarkt.com/club/kader/verein/{tm_id}/saison_id/2023"
+    url = f"https://www.transfermarkt.com/club/kader/verein/{tm_id}/saison_id/{season}"
     pageTree = requests.get(url, headers=headers)
     pageSoup = BeautifulSoup(pageTree.content, 'html.parser')
     #Scrapping all team names
@@ -95,7 +114,9 @@ def scrap_players(tm_id):
     for tr in trs_odd + trs_even:
         number = tr.find("div",{"class" : "rn_nummer"}).text
         table_inline = tr.find("table",{"class":"inline-table"})
-        name = table_inline.find("td",{"class":"hauptlink"}).text.strip()
+        td_name = table_inline.find("td",{"class":"hauptlink"})
+        name = td_name.text.strip()
+        p_tm_id = extract_p_tm_id(td_name.find("a")['href'])
         marketvalue = tr.find("td",{"class" : "rechts hauptlink"}).text.strip()
         
         tds = tr.find_all("td",{"class" : "zentriert"})[1:3]
@@ -105,9 +126,9 @@ def scrap_players(tm_id):
             "number" :number,
             "market_value" : treat_market_value(marketvalue),
             "age" : age,
-            "nat" : nat
+            "nat" : nat,
+            "tm_id" : p_tm_id,
         }
-
     return players
 
 def scrap_comp_info(comp_code):
@@ -127,10 +148,10 @@ def scrap_comp_info(comp_code):
         td = tr.find("td", {"class" : "hauptlink no-border-links"})
         team_name = td.text
         a = td.find("a")
-        tm_id = extract_tm_id(a['href'])
+        tm_id,season = extract_tm_id(a['href'])
         official_name = scrap_official_name(tm_id)
         print(f"Scrapping team: {official_name}")
-        players = scrap_players(tm_id)
+        players = scrap_players(tm_id,season)
         comp[team_name] = {
             "players" : players,
             "tm_id" : tm_id,
@@ -151,11 +172,92 @@ def treat_name(name):
     name = name.replace("'","")
     return name.replace("-"," ")
 
-def main(db_handler):
+def find_player_by_complete_name(p_tm_id : int, current_team:int):
+    player_complete_name = scrap_player_name(p_tm_id)
+    player_complete_name = player_complete_name.replace(" ","%")
+    select_players3 = db_handler.select("player","idplayer,birth_date,name",f"where {like('name',f'%{player_complete_name}%')} and current_team={current_team}")
+    players_selected = select_players3 if select_players3 else []
+    return players_selected
+
+def find_player_by_name(player_name : str, current_team : int):
+    #Se nao encontrou então procurar pelo nome completo
+    player_name = treat_name(player_name)
+    player_names = player_name.split(" ")
+    if(len(player_names) == 1):
+        select_players2 = db_handler.select("player","idplayer,birth_date,name",f"where ({like_multiple('name',[f'%{player_name}%',f'{player_name}%',f'%{player_name}'])}) and current_team={current_team}")
+        players_selected = select_players2 if select_players2 else []
+    else:
+        comparation = f"%{player_names[0]}"
+        for n in player_names[1:]:
+            comparation+= f"%{n}"
+        comparation += "%"
+        select_players2 = db_handler.select("player","idplayer,birth_date,name",f"where {like('name',f'{comparation}')} and current_team={current_team}")
+        players_selected = select_players2 if select_players2 else []
+    return players_selected
+
+def find_player_by_short_name(player_name : str, current_team : int):
+    player_name = treat_name(player_name)
+    player_names = player_name.split(" ")
+    if (len(player_names) == 1):
+        select_players = db_handler.select("player","idplayer,birth_date,short_name",f"where ({like('short_name',f'%{player_name}%')}) and current_team={current_team}")
+        players_selected = select_players if select_players else []
+    elif (len(player_names) == 2):
+        player_name = f"{player_names[0]}%{player_names[1]}"
+        player_name2 = f"{player_names[0][0]}.%{player_names[1]}"
+        player_name3 = f"{player_names[1][0]}.%{player_names[0]}"
+        player_name4 = f"{player_names[1]}%{player_names[0]}"
+        select_players = db_handler.select("player","idplayer,birth_date,short_name",f"where ({like_multiple('short_name',[f'%{player_name}%',f'%{player_name2}%',f'%{player_name3}%',f'%{player_name4}%'])}) and current_team={current_team}")
+        players_selected = select_players if select_players else []
+    else:
+        player_name2 = f"{player_names[0][0]}."
+        for n in player_names[1:]:
+            player_name2 += f" {n}"
+        select_players = db_handler.select("player","idplayer,birth_date,short_name",f"where ({like_multiple('short_name',[f'%{player_name}%',f'%{player_name2}%'])}) and current_team={current_team}")
+        players_selected = select_players if select_players else []
+    return players_selected
+
+def test_players_selected(players_selected : list, assigned_players : list, player_age : int):
+    for player_select in players_selected:
+        id,birth_date,name = player_select
+        if id in assigned_players or player_age != get_age(birth_date):
+            players_selected.remove(player_select)
+
+def update_market_value(players_selected : list, assigned_players : list, player_market_value : int, player_name : str):
+    if(len(players_selected) == 1):
+        #Dar update do valor de mercado
+        assigned_players.append(players_selected[0][0])
+        db_handler.update("player", "market_value", player_market_value, f"WHERE idplayer = {players_selected[0][0]}")
+        return True
+    elif(len(players_selected) > 0):
+        #Comparar e ver qual deles é o real
+        max_similarity = 0
+        actual_id = None
+        found = False
+        for id,_,short_name in players_selected:
+            actual_similarity = similarity(short_name,player_name)
+            if actual_similarity == 1:
+                found = True
+                #Dar update do valor de mercado
+                assigned_players.append(id)
+                db_handler.update("player", "market_value", player_market_value, f"WHERE idplayer = {id}")
+                break
+            elif actual_similarity > max_similarity:
+                actual_id = id
+        if not found:
+            #Escolher o com maior similiradidade de short name
+            #Dar update do valor de mercado
+            assigned_players.append(actual_id)
+            db_handler.update("player", "market_value", player_market_value, f"WHERE idplayer = {actual_id}")
+        return True
+    else:
+        return False
+
+def main(db_handler, cn : bool):
+    start_time = time.time()
     #geting tm code of all competitions in bd
     comp_codes = db_handler.select("competition","idcompetitions,custom_name", log=True)
+    #comp_codes = [(707, 'PO1')]
     not_found_players_file = open("nfp.txt","w+")
-    log_file = open(f"stats.txt","w+")
 
     for (comp_id,comp_code) in comp_codes:
         comp_info = scrap_comp_info(comp_code)
@@ -172,13 +274,9 @@ def main(db_handler):
        
         select_competitions_seasons = db_handler.select("competition_season","top 1 idcompetition_season", f"where competition = {comp_id} order by startDate desc",log=False)
         competition_season = select_competitions_seasons[0][0] if select_competitions_seasons else None
-        
-        players_found = 0
-        players_to_compare = 0
-        players_not_found = 0
+
 
         if competition_season:
-            log_file.write(f"COMPETITION------{comp_code}:\n")
             select_teams_ids = db_handler.select("team_competition_season","team",f"where competition_season = {competition_season}", log=False)
             teams_ids = [id_team for (id_team,) in select_teams_ids] if select_teams_ids else []
             for team_id in teams_ids:
@@ -215,109 +313,23 @@ def main(db_handler):
                 assigned_players = []
                 for player_name,player_info in players.items():
                     player_name = treat_name(player_name)
-                    player_names = player_name.split(" ")
                     player_age = int(player_info['age'])
                     player_market_value = player_info['market_value']
-                    if (len(player_names) == 1):
-                        select_players = db_handler.select("player","idplayer,birth_date,short_name",f"where ({like('short_name',f'%{player_name}%')}) and current_team={current_team}")
-                        players_selected = select_players if select_players else []
-                    elif (len(player_names) == 2):
-                        player_name = f"{player_names[0]}%{player_names[1]}"
-                        player_name2 = f"{player_names[0][0]}.%{player_names[1]}"
-                        player_name3 = f"{player_names[1][0]}.%{player_names[0]}"
-                        player_name4 = f"{player_names[1]}%{player_names[0]}"
-                        select_players = db_handler.select("player","idplayer,birth_date,short_name",f"where ({like_multiple('short_name',[f'%{player_name}%',f'%{player_name2}%',f'%{player_name3}%',f'%{player_name4}%'])}) and current_team={current_team}")
-                        players_selected = select_players if select_players else []
-                    else:
-                        player_name2 = f"{player_names[0][0]}."
-                        for n in player_names[1:]:
-                            player_name2 += f" {n}"
-                        select_players = db_handler.select("player","idplayer,birth_date,short_name",f"where ({like_multiple('short_name',[f'%{player_name}%',f'%{player_name2}%'])}) and current_team={current_team}")
-                        players_selected = select_players if select_players else []
-                    for player_select in players_selected:
-                        id,birth_date,short_name = player_select
-                        if id in assigned_players or player_age != get_age(birth_date):
-                            players_selected.remove(player_select)
-                    if(len(players_selected) == 1):
-                        players_found +=1
-                        #Dar update do valor de mercado
-                        assigned_players.append(players_selected[0][0])
-                        db_handler.update("player", "market_value", player_market_value, f"WHERE idplayer = {players_selected[0][0]}")
-                    elif(len(players_selected) > 0):
-                        players_to_compare += 1
-                        #Comparar e ver qual deles é o real
-                        max_similarity = 0
-                        actual_id = None
-                        found = False
-                        for id,_,short_name in players_selected:
-                            actual_similarity = similarity(short_name,player_name)
-                            if actual_similarity == 1:
-                                found = True
-                                players_found +=1
-                                #Dar update do valor de mercado
-                                assigned_players.append(id)
-                                db_handler.update("player", "market_value", player_market_value, f"WHERE idplayer = {id}")
-                                break
-                            elif actual_similarity > max_similarity:
-                                actual_id = id
-                        if not found:
-                            #Escolher o com maior similiradidade de short name
-                            players_found +=1
-                            #Dar update do valor de mercado
-                            assigned_players.append(actual_id)
-                            db_handler.update("player", "market_value", player_market_value, f"WHERE idplayer = {actual_id}")
-                    else:
+                    #try to find player by short_name
+                    players_selected = find_player_by_short_name(player_name,current_team)
+                    test_players_selected(players_selected, assigned_players, player_age)
+                    if not update_market_value(players_selected, assigned_players, player_market_value, player_name):
                         #Se nao encontrou então procurar pelo nome completo
-                        if(len(player_names) == 1):
-                            select_players2 = db_handler.select("player","idplayer,birth_date,name",f"where ({like_multiple('name',[f'% {player_name} %',f'{player_name} %',f'% {player_name}'])}) and current_team={current_team}")
-                            players_selected = select_players2 if select_players2 else []
-                        else:
-                            comparation = f"%{player_names[0]}"
-                            for n in player_names[1:]:
-                                comparation+= f"%{n}"
-                            comparation += "%"
-                            select_players2 = db_handler.select("player","idplayer,birth_date,name",f"where {like('name',f'{comparation}')} and current_team={current_team}")
-                            players_selected = select_players2 if select_players2 else []
-                        for player_select in players_selected:
-                            id,birth_date,name = player_select
-                            if id in assigned_players or player_age != get_age(birth_date):
-                                players_selected.remove(player_select)
-                        if(len(players_selected) == 1):
-                            players_found +=1
-                            assigned_players.append(players_selected[0][0])
-                            #Dar update do valor de mercado
-                            db_handler.update("player", "market_value", player_market_value, f"WHERE idplayer = {players_selected[0][0]}")
-                        elif(len(players_selected) > 0):
-                            players_to_compare += 1
-                            #Comparar e ver qual deles é o real
-                            max_similarity = 0
-                            actual_id = None
-                            found = False
-                            for id,_,name in players_selected:
-                                actual_similarity = similarity(name,player_name)
-                                if actual_similarity == 1:
-                                    found = True
-                                    players_found +=1
-                                    #Dar update do valor de mercado
-                                    assigned_players.append(id)
-                                    db_handler.update("player", "market_value", player_market_value, f"WHERE idplayer = {id}")
-                                    break
-                                elif actual_similarity > max_similarity:
-                                    actual_id = id
-                            if not found:
-                                #Escolher o com maior similiradidade de name
-                                players_found +=1
-                                #Dar update do valor de mercado
-                                assigned_players.append(actual_id)
-                                db_handler.update("player", "market_value", player_market_value, f"WHERE idplayer = {actual_id}")
-                        else:
-                            not_found_players_file.write(f"{player_name} not found with club: {tm_team_name}\n")
-                            players_not_found +=1
-            log_file.write(f"COMPETITION---{comp_code}\n")
-            log_file.write(f"PF: {players_found}\n")
-            log_file.write(f"PTC: {players_to_compare}\n")
-            log_file.write(f"PNF: {players_not_found}\n")
-
+                        players_selected = find_player_by_name(player_name,current_team)
+                        test_players_selected(players_selected, assigned_players, player_age)
+                        if not update_market_value(players_selected, assigned_players, player_market_value, player_name):
+                            if cn:
+                                players_selected = find_player_by_complete_name(player_info['tm_id'],current_team)
+                                if not update_market_value(players_selected, assigned_players, player_market_value, player_name):
+                                    not_found_players_file.write(f"{player_name} not found with club {tm_team_name}\n")
+                            else:
+                                not_found_players_file.write(f"{player_name} not found with club {tm_team_name}\n")
+    print(f'Threaded: Finished in {time.time()-start_time} seconds')
 
 if __name__ == '__main__':
     args = parse_arguments()
@@ -329,7 +341,7 @@ if __name__ == '__main__':
         db_handler = Db_handler(config_json=db_config_path,logger=db_logger)
         db_handler.create_connection()
         if db_handler.connection:
-            main(db_handler)
+            main(db_handler,args.complete_name)
             db_handler.close_connection()
         else:
             print('DB connection failed to be established.')
