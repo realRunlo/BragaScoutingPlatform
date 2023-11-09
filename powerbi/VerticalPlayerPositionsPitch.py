7f017f2ac593bd6d239c4762aadb36a39c802502
simaocunha71 <a93262@alunos.uminho.pt>
2023-11-09 15:50:28 +0000

    update na view de competiçoes

--- powerbi/VerticalPlayerPositionsPitch.py
+++ powerbi/VerticalPlayerPositionsPitch.py
@@ -11,13 +11,13 @@ def write_text_on_labels_from_powerbi(positions, dataset, ax):
         
         if label in dataset['code'].values:
             percent_value = dataset[dataset['code'] == label]['percent'].values[0]
-            ax.scatter(x, y, color='red', s=500, zorder=2)
-            ax.text(x, y + 4, f'{percent_value}%', color='white', ha='center', va='center', fontsize=12)
+            ax.scatter(x, y, color='red', s=2000, zorder=2)
+            ax.text(x, y+6, f'{percent_value}%', color='red', ha='center', va='center', fontsize=12, weight="bold")
         else:
-            ax.scatter(x, y, color='red', s=500, zorder=2)
+            ax.scatter(x, y, color='red', s=2000, zorder=2)
         
         #ax.text(x, y, str(i), color='white', ha='center', va='center', fontsize=12)
-        ax.text(x, y - 4, label, color='white', ha='center', va='center', fontsize=12)
+        ax.text(x, y, label, color='white', ha='center', va='center', fontsize=12)
 
 FIGWIDTH = 7
 FIGHEIGHT = 8
@@ -32,7 +32,7 @@ PAD_SIDES = ((120 + PAD_TOP + PAD_BOTTOM) * FIGWIDTH / FIGHEIGHT - 80) / 2
 pitch = VerticalPitch(pitch_type="wyscout",
               pad_top=PAD_TOP, pad_bottom=PAD_BOTTOM,
               pad_left=PAD_SIDES, pad_right=PAD_SIDES,
-              pitch_color='grass', stripe=True, line_color='white')
+              pitch_color='white', stripe=False, line_color='gray')
 
 GRID_WIDTH, GRID_HEIGHT = pitch.grid_dimensions(figwidth=FIGWIDTH, figheight=FIGHEIGHT,
                                                 nrows=NROWS, ncols=NCOLS,
@@ -46,31 +46,34 @@ fig, ax = pitch.grid(figheight=FIGHEIGHT, grid_width=GRID_WIDTH,
                      endnote_height=ENDNOTE_HEIGHT, axis=True)
 
 LINHA_GR = 1
-LINHA_DEFESAS_Y = 4
-LINHA_MDC_Y = 6
-LINHA_MC_Y = 8
-LINHA_MO_Y = 10
-LINHA_EXTREMOS_Y = 10
-LINHA_PL_Y = 13
+LINHA_DEFESAS_Y = 3
+LINHA_MDC_Y = 5
+LINHA_MC_Y = 7
+LINHA_MO_Y = LINHA_EXTREMOS_Y = 9
+LINHA_PL_Y = 11
+
+LEFT_LINE = 2
+MID_LINE = 7.2
+RIGHT_LINE = MID_LINE + (MID_LINE - LEFT_LINE)
 
 team_positions = [
-                                                        (5.75, LINHA_GR, 'GK'),                                               # Posicao GR
-    (10.5, LINHA_DEFESAS_Y, 'RB'),                      (5.75, LINHA_DEFESAS_Y, 'CB'),           (1, LINHA_DEFESAS_Y, 'LB'),  # Posicao Defesas
-                                                        (5.75, LINHA_MDC_Y, 'DMF'),                                           # Posicao MDC
-                                                        (5.75, LINHA_MC_Y, 'CMF'),                                            # Posicao MC
-                                                        (5.75, LINHA_MO_Y, 'AMF'),                                            # Posicao MO
-                                                                                                 (1, LINHA_EXTREMOS_Y, 'LW'), # Posicao EE
-                                                        (5.75, LINHA_PL_Y, 'CF'),                                             # Posicao PL
-    (10.5, LINHA_EXTREMOS_Y, 'RW')                                                                                            # Posicao ED
+                                                        (MID_LINE, LINHA_GR, 'GK'),                                                       # Posicao GR
+    (RIGHT_LINE, LINHA_DEFESAS_Y, 'RB'),                (MID_LINE, LINHA_DEFESAS_Y, 'CB'),           (LEFT_LINE, LINHA_DEFESAS_Y, 'LB'),  # Posicao Defesas
+                                                        (MID_LINE, LINHA_MDC_Y, 'DMF'),                                                   # Posicao MDC
+                                                        (MID_LINE, LINHA_MC_Y, 'CMF'),                                                    # Posicao MC
+                                                        (MID_LINE, LINHA_MO_Y, 'AMF'),                                                    # Posicao MO
+                                                                                                (LEFT_LINE, LINHA_EXTREMOS_Y, 'LW'),      # Posicao EE
+                                                        (MID_LINE, LINHA_PL_Y, 'CF'),                                                     # Posicao PL
+    (RIGHT_LINE, LINHA_EXTREMOS_Y, 'RW')                                                                                                  # Posicao ED
 ]
 
 import pandas as pd
 
 #Dados de teste
-data = {'code': ['GK', 'RB'],
-        'percent': [40, 50]}
+#data = {'code': ['GK', 'RB'],
+#        'percent': [40, 50]}
 
-dataset = pd.DataFrame(data)
+#dataset = pd.DataFrame(data)
 
 write_text_on_labels_from_powerbi(team_positions, dataset, ax)
 
